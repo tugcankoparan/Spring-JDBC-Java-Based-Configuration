a46203fe4b4183326a979bb64c617c1c3da07a96
Tuğcan <tugcankoparan10@gmail.com>
2018-09-28 10:10:46 +0300

    Spring JDBC Java Based Configuration

---
+++ SpringJDBC/src/main/java/com/tugcankoparan/app/EmployeeDAO.java
@@ -0,0 +1,5 @@
+package com.tugcankoparan.app;
+
+public interface EmployeeDAO {
+	public String getEmployeeName(int id);
+}
---
+++ SpringJDBC/src/main/java/com/tugcankoparan/app/SpringJDBCConfiguration.java
@@ -0,0 +1,34 @@
+package com.tugcankoparan.app;
+import javax.sql.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+public class SpringJDBCConfiguration {
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.h2.Driver");
+		dataSource.setUrl("jdbc:h2:tcp://localhost/~/test");
+		dataSource.setUsername("****");
+		dataSource.setPassword("****");
+		return dataSource;
+	}
+
+	@Bean
+	public JdbcTemplate jdbcTemplate() {
+		JdbcTemplate jdbcTemplate = new JdbcTemplate();
+		jdbcTemplate.setDataSource(dataSource());
+		return jdbcTemplate;
+	}
+
+	@Bean
+	public EmployeeDAO employeeDAO(){
+		EmployeeDAOImpl empDao = new EmployeeDAOImpl();
+		empDao.setJdbcTemplate(jdbcTemplate());
+		return empDao;
+	}
+
+}
